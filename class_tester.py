35539aa35666744271c3d8bd4708123b17139536
copperwire <solli.robert@gmail.com>
2016-02-17 16:44:18 +0100

    0.95 gui file chooser

--- class_tester.py
+++ class_tester.py
@@ -1,5 +1,8 @@
 from plotting_module import *
 from interactive_plotting import *
+import tkinter
+from tkinter import filedialog
+
 """
 filename = "/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/151118h.dp_rpc_asc"
 test = plotter(filename)
@@ -11,8 +14,22 @@ test_2 = plotter(filevname_2)
 test_2.plot_machine()
 """
 
-filename = ["/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/151118h.dp_rpc_asc"
-			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/151112s.dp_rpc_asc"]
+
+def file_location(self, filename):
+	root = tkinter.Tk()
+	files = filedialog.askopenfilenames(parent=root,title='Choose files')
+	files = root.tk.splitlist(files)
+	return files
+
+
+filename = file_location()
+
+"""["/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112b.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112d.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112f.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112h.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/time_x_axis.dp_rpc_asc"]"""
 test = interactive_plotting(filename)
 test.data_generation()
 test.plotting()
+
