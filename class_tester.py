512ba0e1ca3089450bbbcf8d4075a3616b97b236
copperwire <solli.robert@gmail.com>
2016-03-01 12:58:37 +0100

    0.96 gui file chooser

--- class_tester.py
+++ class_tester.py
@@ -1,10 +1,11 @@
 from plotting_module import *
 from interactive_plotting import *
+import sys
 import tkinter
 from tkinter import filedialog
 
 """
-filename = "/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/151118h.dp_rpc_asc"
+filename = "/home/solli/Documents/Prosjektoppgave/data_files/depth_profile/151118h.dp_rpc_asc"
 test = plotter(filename)
 test.plot_machine()
 
@@ -14,21 +15,23 @@ test_2 = plotter(filevname_2)
 test_2.plot_machine()
 """
 
-
-def file_location(self, filename):
+def file_location():
 	root = tkinter.Tk()
 	files = filedialog.askopenfilenames(parent=root,title='Choose files')
 	files = root.tk.splitlist(files)
 	return files
 
 
-filename = file_location()
 
-"""["/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112b.dp_rpc_asc"
-			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112d.dp_rpc_asc"
-			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112f.dp_rpc_asc"
-			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/160112h.dp_rpc_asc"
-			,"/home/solli/Documents/Prosjektoppgave/Data_files/depth_profile/time_x_axis.dp_rpc_asc"]"""
+try: 
+	if sys.argv[1] == "f":
+		filename = ["/home/solli/Documents/Prosjektoppgave/data_files/depth_profile/160112b.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/data_files/depth_profile/160112d.dp_rpc_asc"
+			,"/home/solli/Documents/Prosjektoppgave/data_files/depth_profile/160112f.dp_rpc_asc"]
+except IndexError:
+	filename = file_location()
+
+
 test = interactive_plotting(filename)
 test.data_generation()
 test.plotting()
